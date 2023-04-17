20c5e77ee8b2ce3424afb011b1cef3bb844b278c
Dima <dimasakharov@icloud.com>
2023-04-17 18:11:10 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ygamijs.github.io",
+  base: "/Dima",
   site: SITE.website,
   integrations: [
     tailwind({
