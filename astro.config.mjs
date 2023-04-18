928c460f4e7ebe836941b71336c9a9524d610a16
Dima <dimasakharov@icloud.com>
2023-04-18 16:49:07 +0300

    add hightlight code

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: "min-dark",
       wrap: true,
     },
     extendDefaultPlugins: true,
